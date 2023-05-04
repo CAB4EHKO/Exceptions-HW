9c76e37e2687bd9730f238592902eed2b789fd9d
Savchenko <savchenko.ra@yandex.ru>
2023-05-04 11:37:35 +0300

    Final commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!11");
+        try {
+            RegistrationService.registrationForm("Kak_budda", "Sky2023Pro", "Sky2023Pro");
+        } catch (InvalidCharacterException e) {
+            System.out.println("Логин и пароль могут содержать только латинские буквы, цифры и знак подчёркивания.");
+        } catch (WrongLoginException e) {
+            System.out.println("Логин и пароль должны быть не длиннее 20 символов.");
+        } catch (WrongPasswordException e) {
+            System.out.println("Пароли не совподает.");
+        }
     }
 }
